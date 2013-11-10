25f4b1095d405eb24496e25c3effa56d22ae753a
kish3ore@gmail.com <kish3ore@gmail.com@602cd602-cdb6-65c5-99e5-5569ff72161f>
2013-11-10 03:11:35 +0000

    Adding Junits for Category and Event classes

---
+++ trunk/mycalendarApp/src/com/example/mycalendarapp/TestCategory.java
@@ -0,0 +1,28 @@
+package com.example.mycalendarapp;
+
+import junit.framework.TestCase;
+
+public class TestCategory extends TestCase {
+	
+	Category Cat=new Category("TestCat","blue");
+	
+	public void testToString() {
+		assertEquals(Cat.toString(),"Category [id=0, name=TestCat, color=blue]");
+	}
+
+	public void testGetSetId() {
+		Cat.setId(1510);
+		assertEquals(Cat.getId(),1510);
+	}
+
+	public void testGetSetName() {
+		Cat.setName("Shopping");
+		assertEquals(Cat.getName(),"Shopping");
+	}
+
+	public void testGetSetColor() {
+		Cat.setColor("yellow");
+		assertEquals(Cat.getColor(),"yellow");
+	}
+
+}
